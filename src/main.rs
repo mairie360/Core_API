d5d8be5093eefa4b9d220cf52afcb18220be3d6a
Quentin Tennerel <tennerelquentin@gmail.com>
2025-06-10 21:45:37 +0200

    Feat: Add base of server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello World!");
+use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+
+#[post("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(|| App::new().service(hello)).bind("127.0.0.1:8080")?;
+
+    let addr = server.addrs().first().cloned();
+    tokio::spawn(async move {
+        if let Some(addr) = addr {
+            println!("Serveur démarré avec succès sur http://{}", addr);
+        }
+    });
+
+    server.run().await
 }
