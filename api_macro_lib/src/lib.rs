c41600c81d0666c0d276e665819f9954c28a1246
Quentin Tennerel <tennerelquentin@gmail.com>
2025-07-18 19:00:57 +0200

    feat: check user before timeout for jwt check

--- api_macro_lib/src/lib.rs
+++ api_macro_lib/src/lib.rs
@@ -28,7 +28,52 @@ pub fn check_jwt(_attr: TokenStream, item: TokenStream) -> TokenStream {
             };
             println!("User ID from JWT: {}", user_id);
 
-            // Appel de la fonction originale
+            let view = DoesUserExistByIdQueryView::new(user_id.parse().unwrap());
+            let db_guard = get_db_interface().lock().unwrap();
+            let db_interface = match &*db_guard {
+                Some(db) => db,
+                None => {
+                    eprintln!("Database interface is not initialized.");
+                    return HttpResponse::InternalServerError().body("Internal server error: Database not initialized.");
+                }
+            };
+            let query_view = db_interface.execute_query(Box::new(view)).await;
+
+            let result = match query_view {
+                Ok(result) => result,
+                Err(e) => {
+                    eprintln!("Error executing query: {}", e);
+                    return HttpResponse::InternalServerError().body("Internal server error: Database query failed.");
+                }
+            };
+
+            if !get_boolean_from_query_result(result.get_result()) {
+                eprintln!("User does not exist with ID: {}", user_id);
+                return HttpResponse::NotFound().body("User not found.")
+            }
+
+            let timeout: usize = match get_timeout_from_jwt(&jwt) {
+                Some(t) => t,
+                None => {
+                    eprintln!("Failed to retrieve timeout from JWT.");
+                    return HttpResponse::Unauthorized().body("Unauthorized: Invalid JWT token.");
+                }
+            };
+
+            match verify_jwt_timeout(timeout) {
+                Ok(true) => {
+                    println!("JWT token is valid and not expired.");
+                },
+                Ok(false) => {
+                    eprintln!("JWT token is expired or invalid.");
+                    return HttpResponse::Unauthorized().body("Unauthorized: JWT token is expired or invalid.");
+                },
+                Err(e) => {
+                    eprintln!("Error verifying JWT timeout: {}", e);
+                    return HttpResponse::InternalServerError().body("Internal server error: Failed to verify JWT timeout.");
+                }
+            }
+
             async fn original_logic(req: HttpRequest, path_view: web::Path<AboutPathParamRequestView>) -> HttpResponse {
                 #block
             }
