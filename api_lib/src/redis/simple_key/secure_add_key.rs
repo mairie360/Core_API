b5fc6d060772822a1aaed29b9fd9b3ebe9397940
Quentin Tennerel <tennerelquentin@gmail.com>
2025-07-21 08:42:59 +0200

    fix: fix redis secure add key
    add the key when it doesn't exist

--- api_lib/src/redis/simple_key/secure_add_key.rs
+++ api_lib/src/redis/simple_key/secure_add_key.rs
@@ -4,11 +4,11 @@ use super::key_exist;
 
 pub async fn secure_add_key(conn: &mut Connection, key: &str, value: &str) -> Result<(), redis::RedisError> {
     match key_exist(conn, key).await {
-        Ok(true) => add_key(conn, key, value).await,
-        Ok(false) => Err(redis::RedisError::from((
+        Ok(false) => add_key(conn, key, value).await,
+        Ok(true) => Err(redis::RedisError::from((
             redis::ErrorKind::ResponseError,
-            "Key does not exist",
-            format!("Key '{}' does not exist", key),
+            "Key already exist",
+            format!("Key '{}' already exist", key),
         ))),
         Err(err) => Err(err),
     }
