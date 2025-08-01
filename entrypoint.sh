97b002ef82e81957e25551904af13ce4e786b342
Quentin Tennerel <tennerelquentin@gmail.com>
2025-08-01 17:17:43 +0200

    change: improve entrypoint.sh format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cargo watch -w src -w api_lib -w api_macro_lib -i target -i api_lib/target -i api_macro_lib/target -x run
+cargo watch \
+  -w src \
+  -w api_lib \
+  -w api_macro_lib \
+  -i target \
+  -i api_lib/target \
+  -i api_macro_lib/target \
+  -x run
