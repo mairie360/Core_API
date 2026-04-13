7fbc544bf72566df7ea4bac79daee88da9f2fb5d
Quentin Tennerel <tennerelquentin@gmail.com>
2026-04-13 13:38:56 +0800

    update: update orval config

--- orval.config.js
+++ orval.config.js
@@ -5,7 +5,7 @@ module.exports = {
       mode: 'tags-split', // C'EST LA CLÉ : un fichier par Tag (Roles, Sessions, etc.)
       target: 'generated/endpoints',
       schemas: 'generated/model', // Met tous les types/interfaces ici
-      client: 'react-query', // ou 'fetch', 'axios' selon ton besoin
+      client: 'axios', // ou 'fetch', 'axios' selon ton besoin
       mock: false,
     },
   },
